11ad1f490c1baff857a66d05aca806c817cfa4bb
Artur Morais <arturpmrs@gmail.com>
2023-01-14 23:33:31 -0300

    Movendo repositórios para pastas específicas

--- src/main/java/com/example/clientscrud/repository/ClientGroupRepository.java
+++ src/main/java/com/example/clientscrud/repository/ClientGroupRepository.java
@@ -1,8 +1,15 @@
-package com.example.clientscrud.model;
+package com.example.clientscrud.repository;
+
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import com.example.clientscrud.model.ClientGroup;
+
 @RepositoryRestResource(collectionResourceRel = "clientGroups", path = "clientGroups")
 public interface ClientGroupRepository extends JpaRepository<ClientGroup, Long> {
+    void deleteClientGroupById(Long id);
+
+    Optional<ClientGroup> findClientGroupById(Long id);
 }
--- src/main/java/com/example/clientscrud/repository/ClientRepository.java
+++ src/main/java/com/example/clientscrud/repository/ClientRepository.java
@@ -1,8 +1,15 @@
-package com.example.clientscrud.model;
+package com.example.clientscrud.repository;
+
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import com.example.clientscrud.model.Client;
+
 @RepositoryRestResource(collectionResourceRel = "clients", path = "clients")
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    void deleteClientById(Long id);
+
+    Optional<Client> findClientById(Long id);
 }
