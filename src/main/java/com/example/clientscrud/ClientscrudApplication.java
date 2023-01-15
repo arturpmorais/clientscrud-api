866d041d7f27db46f87700a8fdb01b671f3cc510
Artur Morais <arturpmrs@gmail.com>
2023-01-15 00:32:23 -0300

    Config para retornar id nas apis

--- src/main/java/com/example/clientscrud/ClientscrudApplication.java
+++ src/main/java/com/example/clientscrud/ClientscrudApplication.java
@@ -5,16 +5,28 @@ import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.example.clientscrud.model.Client;
+import com.example.clientscrud.model.ClientGroup;
 
 @SpringBootApplication
-public class ClientscrudApplication {
+public class ClientscrudApplication implements RepositoryRestConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientscrudApplication.class, args);
 	}
+	
+	@Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(ClientGroup.class);
+        config.exposeIdsFor(Client.class);
+    }
 
 	@Bean
 	public CorsFilter corsFilter() {
